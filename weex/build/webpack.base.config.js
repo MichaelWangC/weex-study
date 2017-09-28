553c310e347ac53114cdbbc181116f4cbcd30794
MichaelWangC <623572368@qq.com>
2017-09-28 11:54:41 +0800

    weex多入口打包

--- weex/build/webpack.base.config.js
+++ weex/build/webpack.base.config.js
@@ -1,10 +1,43 @@
-var path = require('path')
+var pathTo = require('path')
 var webpack = require('webpack')
 var merge = require('webpack-merge')
 var CleanWebpackPlugin = require('clean-webpack-plugin')
 var vueLoaderConfig = require('./vue-loader.config.js')
 var config = require('../config')
 
+/// 入口文件配置
+const weexEntry = {};
+const vueWebTemp = 'entrys';
+const fs = require('fs-extra')
+// const hasPluginInstalled = fs.existsSync('./web/plugin.js')
+var resolve = require('../build/utils').resolve
+
+// 获取入口文件 配置
+function walk(dir) {
+  dir = dir || '.';
+  const directory = pathTo.join(resolve('src'), dir); // /src,/src/components
+  fs.readdirSync(directory).forEach(function(file) {
+
+    const fullpath = pathTo.join(directory, file); // /src/App.vue,/src/components,/src/components/Hello.vue
+    const stat = fs.statSync(fullpath);
+    const extname = pathTo.extname(fullpath);
+    if (stat.isFile() && extname === '.vue') {
+      /// 生成入口文件
+      const name = pathTo.join(dir, pathTo.basename(file, extname)); // App Hello
+
+      weexEntry[name] = fullpath + '?entry=true';
+
+    } else if (stat.isDirectory() && file !== 'components' && file !== 'assets') {
+      const subdir = pathTo.join(dir, file);
+      walk(subdir);
+    }
+
+  }, this);
+}
+walk();
+
+// console.log('weexEntry:' + JSON.stringify(weexEntry))
+// 基本配置
 var baseConfig = {
   module: {
     rules: [{
@@ -22,7 +55,7 @@ var baseConfig = {
 
 var webConfig = merge(baseConfig, {
   entry: {
-    app: config.build.entryPath.web,
+    App: config.build.entryPath.web
   },
   output: {
     path: config.build.publicWebRoot,
@@ -41,9 +74,7 @@ var webConfig = merge(baseConfig, {
 })
 
 var nativeConfig = merge(baseConfig, {
-  entry: {
-    app: config.build.entryPath.native,
-  },
+  entry: weexEntry,
   output: {
     path: config.build.publicDistRoot,
     // path: resolve('public/dist/weex/'),
