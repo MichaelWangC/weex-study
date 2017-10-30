f283c2cf6deea94d6834074523b95fae9b91c6e0
MichaelWangC <623572368@qq.com>
2017-10-30 21:27:40 +0800

    我的界面 自适应高度

--- weex/src/modules/mine/mine.vue
+++ weex/src/modules/mine/mine.vue
@@ -1,25 +1,34 @@
 <template>
-  <div>
-      <navpage dataRole="none" title="我的" titleColor="white">
-          <div>
-              我的
-          </div>
-      </navpage>
-  </div>
+    <div>
+        <navpage dataRole="none" title="" titleColor="white" rightItemTitle="个人信息" rightItemColor="white">
+            <div class="mine-asset-bg">
+                我的我的我的我的我的我的我的我的我的我的我的我的我的我的我的我的我的
+                
+            </div>
+        </navpage>
+    </div>
 </template>
 
 <script>
 import navpage from '@/components/navpage.vue'
 
 export default {
-  components: {
-      navpage
-  }
+    data() {
+        return {
+
+        }
+    },
+    components: {
+        navpage
+    }
 }
 </script>
 
 <style scoped>
+.mine-asset-bg {
+    width: 750;
+    min-height: 200;
+    background-color: #3399ff;
+}
 
 </style>
-
-
