91f0ee8c956899dcbc3956e943273b4d9b372d36
MichaelWangC <623572368@qq.com>
2017-11-04 18:11:52 +0800

    min-height 测试

--- weex/src/modules/mine/mine.vue
+++ weex/src/modules/mine/mine.vue
@@ -1,34 +1,32 @@
 <template>
     <div>
         <navpage dataRole="none" title="" titleColor="white" rightItemTitle="个人信息" rightItemColor="white">
-            <div class="mine-asset-bg">
-                我的我的我的我的我的我的我的我的我的我的我的我的我的我的我的我的我的
-                
-            </div>
+            <scroller>
+                <div class="mine-asset-bg">
+                    我的
+                </div>
+            </scroller>
         </navpage>
     </div>
 </template>
 
 <script>
-import navpage from '@/components/navpage.vue'
+import navpage from "@/components/navpage.vue";
 
 export default {
-    data() {
-        return {
-
-        }
-    },
-    components: {
-        navpage
-    }
-}
+  data() {
+    return {};
+  },
+  components: {
+    navpage
+  }
+};
 </script>
 
 <style scoped>
 .mine-asset-bg {
-    width: 750;
-    min-height: 200;
-    background-color: #3399ff;
+  width: 750;
+  min-height: 200;
+  background-color: #3399ff;
 }
-
 </style>
